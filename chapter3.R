863a6e9a7ff15975c356e48eec1cd8f6effcbcea
lan1324 <ganghyeon21@gmail.com>
2025-03-22 14:47:38 +0900

    Create chapter3.R

---
+++ chapter3.R
@@ -0,0 +1,25 @@
+a<-1
+b<-2
+a+b
+
+str1<-"a"
+str2<-"Hello World!"
+str1
+str2
+
+var1<-c(1,2,5,7,8)
+var2<-c(1:5)
+var3<-seq(1,5)
+var4<-seq(1,10, by=2)
+var1
+var2
+var3
+var4
+var1+2
+var1+var2
+
+str4<-c("apple", "banana", "carrot")
+str5<-c("a", "b", "c")
+str4
+str5
+str4+str5
