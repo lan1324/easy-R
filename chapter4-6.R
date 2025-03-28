b8a16a970be8e2df066ee166eecba7dada2a8644
lan1324 <ganghyeon21@gmail.com>
2025-03-28 20:00:15 +0900

    Update chapter4-6.R

--- chapter4-6.R
+++ chapter4-6.R
@@ -100,3 +100,28 @@ View(exam) ## 뷰어창에서 데이터 확인 - 맨 앞이 대문자 V
 
 dim(exam) ## 행, 열 출력
 ## [1] 20  5
+
+str(exam) ##모든 변수의 속성을 한눈에 파악 가능
+## 'data.frame':	20 obs. of  5 variables:
+## $ id     : int  1 2 3 4 5 6 7 8 9 10 ...
+## $ class  : int  1 1 1 1 2 2 2 2 3 3 ...
+## $ math   : int  50 60 45 30 25 50 80 90 20 50 ...
+## $ english: int  98 97 86 98 80 89 90 78 98 98 ...
+## $ science: int  50 60 78 58 65 98 45 25 15 45 ...
+
+summary(exam)
+## id            class        math          english        science     
+## Min.   : 1.00   Min.   :1   Min.   :20.00   Min.   :56.0   Min.   :12.00 
+## 1st Qu.: 5.75   1st Qu.:2   1st Qu.:45.75   1st Qu.:78.0   1st Qu.:45.00 
+##1st Qu - 하위 25%에 위치하는 값
+## Median :10.50   Median :3   Median :54.00   Median :86.5   Median :62.50  
+## Median - 중앙값
+## Mean   :10.50   Mean   :3   Mean   :57.45   Mean   :84.9   Mean   :59.45  
+## 3rd Qu.:15.25   3rd Qu.:4   3rd Qu.:75.75   3rd Qu.:98.0   3rd Qu.:78.00
+## 3rd Qu - 하위 75%지점에 위치하는 값
+## Max.   :20.00   Max.   :5   Max.   :90.00   Max.   :98.0   Max.   :98.00 
+
+install.packages("ggplot2")
+mpg<-as.data.frame(ggplot2::mpg) ## mpg데이터를 데이터 프레임 형태로 불러옴
+## ggplot2::mpg<< ggplot2 속의 mpg데이터를 지칭하는 코드
+
