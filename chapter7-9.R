9a22b0a6c69babac1709773ff6217c78b542ff10
lan1324 <ganghyeon21@gmail.com>
2025-04-01 11:54:44 +0900

    Update chapter7-9.R

--- chapter7-9.R
+++ chapter7-9.R
@@ -1,3 +1,5 @@
+##-----------------chapter 7 ------------
+## 7-1데이터정제
 df <- data.frame(sex=c("M", "F", NA, "M", "F"), score=c(5, 4, 3, 4, NA)) #따옴표가 없으면 결측치
 is.na(df)
 ## sex score
@@ -25,3 +27,32 @@ mean(df$score) #평균산출
 sum(df$score)
 #[1] NA < 합계도 정상적으로 산출되지 않는다
 
+library(dplyr)
+df %>% filter(is.na(score)) #score가 NA인 데이터만 출력
+## sex score
+## 1   F    NA
+
+df %>% filter(!is.na(score)) #score 결측치(NA) 제거된 데이터만 출력
+df_nomiss<-df %>% filter(!is.na(score)) #score 결측치 제거
+mean(df_nomiss$score)
+## [1] 4
+sum(df_nomiss$score)
+## [1] 16
+df_nomiss<-df%>%filter(!is.na(score)&!is.na(sex)) ##score, sex결측치동시제거
+
+df_nomiss2<-na.omit(df) #변수지정없이 결측치 없는 데이터 추출 <<편하지만 데이터의 지나친 소실 우려
+
+mean(df$score, na.rm=T) #결측치 제외하고 평균산출
+## [1] 4
+sum(df$score, na.rm=T) #결측치 제외하고 합계산출
+## [1] 16
+exam<-read.csv("C:/Users/gangh/Desktop/easy_r/R/csv_exam.csv")
+exam[c(3, 8, 15), "math"] <- NA ##3, 8, 5행의 math에 NA 할당
+exam %>% summarise(mean_math=mean(math))
+## mean_math
+## 1        NA
+
+#math 결측치 제외하고 값 산출
+exam %>% summarise(mean_math=mean(math, na.rm=T), sum_math=sum(math, na.rm=T), median_math=median(math, na.rm=T))
+## mean_math sum_math median_math
+## 1  55.23529      939          50
