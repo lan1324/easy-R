6a3fa5fc84ac50abaf4a1306d3f02b88614d5500
lan1324 <ganghyeon21@gmail.com>
2025-04-03 21:34:33 +0900

    Update chapter7-9.R

--- chapter7-9.R
+++ chapter7-9.R
@@ -109,3 +109,38 @@ mpg %>%
 
 ## ----------------------chapter 8----------
 ## 8-2 산점도-변수간 관계 표현하기
+library(ggplot2)
+
+#x축은 displ, y축은 hwy로 지정해 배경생성
+ggplot(data=mpg, aes(x=displ, y=hwy))
+
+## 배경에 산점도 추가
+ggplot(data=mpg, aes(x=displ, y=hwy)) + geom_point()
+
+## x축 범위 3-6, y축 범위 10-30으로 지정
+ggplot(data=mpg, aes(x=displ, y=hwy)) + geom_point() +
+  xlim(3, 6) +
+  ylim(10, 30)
+
+## 데이터 프레임 생성
+library(dplyr)
+df_mpg<-mpg %>%
+  group_by(drv) %>%
+  summarise(mean_hwy=mean(hwy))
+df_mpg
+
+## 그래프 생성
+ggplot(data=df_mpg, aes(x=drv, y=mean_hwy)) + geom_col()
+ggplot(data=df_mpg, aes(x=reorder(drv, -mean_hwy), y=mean_hwy)) + geom_col()
+
+##빈도그래프
+ggplot(data=mpg, aes(x=drv)) + geom_bar()
+ggplot(data=mpg, aes(x=hwy)) + geom_bar()
+
+ggplot(data=economics, aes(x=date, y=unemploy)) + geom_line()
+
+ggplot(data=mpg, aes(x=drv, y=hwy)) + geom_boxplot()
+
+
+## -------------------chapter 9------------
+#9-1 한국복지패널데이터 분석석
