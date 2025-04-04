60aa6483b44f4f55bc5625aa580054b86b6a33bf
lan1324 <ganghyeon21@gmail.com>
2025-04-04 21:34:12 +0900

    Update chapter7-9.R

--- chapter7-9.R
+++ chapter7-9.R
@@ -286,4 +286,27 @@ ggplot(data=bottom10, aes(x=reorder(job, -mean_income), y=mean_income)) +
 
 ##9-7성별 직업 빈도
 #남성 직업 빈도 상위 10개 추출
-job_
+job_male<-welfare %>%
+  filter(!is.na(job)&sex=="male") %>%
+  group_by(job) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n)) %>%
+  head(10)
+
+#여성 직업 빈도 상위 10개 추출
+job_female<-welfare %>%
+  filter(!is.na(job)&sex=="female") %>%
+  group_by(job) %>%
+  summarise(n=n()) %>%
+  arrange(desc(n)) %>%
+  head(10)
+
+#남성 직업 빈도 상위 10개 그래프
+ggplot(data=job_male, aes(x=reorder(job,n), y=n)) +
+         geom_col() +
+         coord_flip()
+
+#여성 직업 빈도 상위 10개 그래프
+ggplot(data=job_female, aes(x=reorder(job, n), y=n)) +
+  geom_col() +
+  coord_flip()
